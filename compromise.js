41b2e12309d1cedd3ed2613f88228d6ccd8395c9
Lin Zhu <linuszhu@gmail.com>
2017-08-21 11:45:01 -0700

    remove unused script

--- compromise.js
+++
@@ -1,2 +0,0 @@
-var nlp = require('compromise')
-var parsed = nlp('Now this is a story all about how..')
--- index.js
+++
@@ -1,6 +0,0 @@
-var speak = require("./speakeasy-nlp");
-
-// Analyze sentences at a basic level
-// ------------------------------------- //
-speak.classify("What is your name?")             //=> { action: "what", owner: "listener", subject: "name" }
-speak.classify("Do you know what time it is?")   //=> { action: "what", owner: "it", subject: "time" }
